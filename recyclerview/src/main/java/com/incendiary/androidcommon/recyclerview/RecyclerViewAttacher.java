77549546610af0b6a31d88ffebda27bff249ba89
Esa Firman <esafirm21@gmail.com>
2016-09-27 13:50:31 +0700

    Change ItemDecoration to be Varargs for multiple decoration

--- recyclerview/src/main/java/com/incendiary/androidcommon/recyclerview/RecyclerViewAttacher.java
+++ recyclerview/src/main/java/com/incendiary/androidcommon/recyclerview/RecyclerViewAttacher.java
@@ -6,54 +6,57 @@ import android.support.v7.widget.RecyclerView;
 /**
  * Created by esafirm on 8/9/16.
  */
-public class RecyclerViewAttacher {
-
-    public static RecyclerAttacherBuilder with(RecyclerView recyclerView) {
-        return new RecyclerAttacherBuilder(recyclerView);
+public static RecyclerAttacherBuilder with(RecyclerView recyclerView) {
+    return new RecyclerAttacherBuilder(recyclerView);
     }
 
-    public static class RecyclerAttacherBuilder {
+public static class RecyclerAttacherBuilder {
 
-        private RecyclerView recyclerView;
-        private RecyclerView.LayoutManager layoutManager;
-        private RecyclerView.Adapter adapter;
-        private RecyclerView.ItemDecoration itemDecoration;
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.ItemDecoration[] itemDecorations;
 
-        private boolean hasFixedSize;
+    private boolean hasFixedSize;
 
-        RecyclerAttacherBuilder(RecyclerView recyclerView) {
-            this.recyclerView = recyclerView;
-            hasFixedSize = true;
+    RecyclerAttacherBuilder(RecyclerView recyclerView) {
+        this.recyclerView = recyclerView;
+        hasFixedSize = true;
+    }
+
+    public RecyclerAttacherBuilder hasFixedSize(boolean hasFixedSize) {
+        this.hasFixedSize = hasFixedSize;
+        return this;
+    }
+
+    public RecyclerAttacherBuilder layoutManager(RecyclerView.LayoutManager manager) {
+        this.layoutManager = manager;
+        return this;
+    }
+
+    public RecyclerAttacherBuilder adapter(RecyclerView.Adapter adapter) {
+        this.adapter = adapter;
+        return this;
+    }
+
+    public RecyclerAttacherBuilder itemDecoration(RecyclerView.ItemDecoration... itemDecoration) {
+        this.itemDecorations = itemDecoration;
+        return this;
+    }
+
+    public void build() {
+        if (layoutManager == null) {
+            layoutManager = new LinearLayoutManager(recyclerView.getContext());
         }
 
-        public RecyclerAttacherBuilder hasFixedSize(boolean hasFixedSize) {
-            this.hasFixedSize = hasFixedSize;
-            return this;
-        }
-
-        public RecyclerAttacherBuilder layoutManager(RecyclerView.LayoutManager manager) {
-            this.layoutManager = manager;
-            return this;
-        }
-
-        public RecyclerAttacherBuilder adapter(RecyclerView.Adapter adapter) {
-            this.adapter = adapter;
-            return this;
-        }
-
-        public RecyclerAttacherBuilder itemDecoration(RecyclerView.ItemDecoration itemDecoration) {
-            this.itemDecoration = itemDecoration;
-            return this;
-        }
-
-        public void build() {
-            if (layoutManager == null) {
-                layoutManager = new LinearLayoutManager(recyclerView.getContext());
+        if (itemDecorations != null) {
+            for (RecyclerView.ItemDecoration decoration : itemDecorations) {
+                recyclerView.addItemDecoration(decoration);
             }
-            if (itemDecoration != null) recyclerView.addItemDecoration(itemDecoration);
-            recyclerView.setLayoutManager(layoutManager);
-            recyclerView.setHasFixedSize(hasFixedSize);
-            recyclerView.setAdapter(adapter);
         }
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(hasFixedSize);
+        recyclerView.setAdapter(adapter);
     }
 }

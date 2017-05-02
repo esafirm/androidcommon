4020d171a16eb4b68c2b94a18528d389c5d7efb5
Esa Firman <esafirm21@gmail.com>
2017-05-02 10:34:38 +0700

    Update build tools version
    Add getString to ContextProvider

--- app/src/main/java/com/incendiary/androidcommon/android/ContextProvider.java
+++ app/src/main/java/com/incendiary/androidcommon/android/ContextProvider.java
@@ -5,6 +5,7 @@ import android.graphics.drawable.Drawable;
 import android.support.annotation.ColorInt;
 import android.support.annotation.ColorRes;
 import android.support.annotation.DrawableRes;
+import android.support.annotation.StringRes;
 import android.support.v4.content.ContextCompat;
 
 /**
@@ -62,5 +63,13 @@ public class ContextProvider {
 
     public static Drawable getDrawable(@DrawableRes int drawable) {
         return ContextCompat.getDrawable(get(), drawable);
+    }
+
+    public static String getString(@StringRes int stringRes) {
+        return get().getString(stringRes);
+    }
+
+    public static String getString(@StringRes int stringRes, Object... args) {
+        return get().getString(stringRes, args);
     }
 }
